db8e4091928d7b2796add4615e5a260a822e83eb
Daniel Díaz <diaz_carrete@yahoo.com>
2022-05-29 11:43:36 +0200

    Started moving Types.hs

--- lib/SQLite/Query/Types.hs
+++ lib/SQLite/Query/Types.hs
@@ -1,6 +1,12 @@
-{-# LANGUAGE DeriveDataTypeable, DeriveFunctor, GeneralizedNewtypeDeriving, CPP #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeOperators #-}
 
 ------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------
+
 -- |
 -- Module:      Database.SQLite.Simple.Types
 -- Copyright:   (c) 2011 MailRank, Inc.
@@ -11,35 +17,27 @@
 -- Portability: portable
 --
 -- Top-level module for sqlite-simple.
---
---
-------------------------------------------------------------------------------
+module SQLite.Query.Types
+  ( Null (..),
+    Solo (..),
+    Query (..),
+    (:.) (..),
+  )
+where
 
-module Database.SQLite.Simple.Types
-    (
-      Null(..)
-    , Only(..)
-    , Query(..)
-    , (:.)(..)
-    ) where
-
-import           Control.Arrow (first)
-import           Data.String (IsString(..))
-import           Data.Tuple.Only (Only(..))
-import           Data.Typeable (Typeable)
+import Control.Arrow (first)
+import Data.String (IsString (..))
 import qualified Data.Text as T
-
-#if !MIN_VERSION_base(4,11,0)
-import Data.Semigroup
-#endif
+import Data.Tuple (Solo (..))
+import Data.Typeable (Typeable)
 
 -- | A placeholder for the SQL @NULL@ value.
 data Null = Null
-          deriving (Read, Show, Typeable)
+  deriving (Read, Show, Typeable)
 
 instance Eq Null where
-    _ == _ = False
-    _ /= _ = False
+  _ == _ = False
+  _ /= _ = False
 
 -- | A query string. This type is intended to make it difficult to
 -- construct a SQL query by concatenating string fragments, as that is
@@ -59,27 +57,28 @@ instance Eq Null where
 --
 -- The underlying type is a 'Text', and literal Haskell strings that
 -- contain Unicode characters will be correctly transformed to UTF-8.
-newtype Query = Query {
-      fromQuery :: T.Text
-    } deriving (Eq, Ord, Typeable)
+newtype Query = Query
+  { fromQuery :: T.Text
+  }
+  deriving (Eq, Ord, Typeable)
 
 instance Show Query where
-    show = show . fromQuery
+  show = show . fromQuery
 
 instance Read Query where
-    readsPrec i = fmap (first Query) . readsPrec i
+  readsPrec i = fmap (first Query) . readsPrec i
 
 instance IsString Query where
-    fromString = Query . T.pack
+  fromString = Query . T.pack
 
 instance Semigroup Query where
-    Query a <> Query b = Query (T.append a b)
-    {-# INLINE (<>) #-}
+  Query a <> Query b = Query (T.append a b)
+  {-# INLINE (<>) #-}
 
 instance Monoid Query where
-    mempty = Query T.empty
-    mappend = (<>)
-    {-# INLINE mappend #-}
+  mempty = Query T.empty
+  mappend = (<>)
+  {-# INLINE mappend #-}
 
 -- | A composite type to parse your custom data structures without
 -- having to define dummy newtype wrappers every time.
@@ -97,6 +96,6 @@ instance Monoid Query where
 -- forM res $ \\(MyData{..} :. MyData2{..}) -> do
 --   ....
 -- @
-data h :. t = h :. t deriving (Eq,Ord,Show,Read,Typeable)
+data h :. t = h :. t deriving (Eq, Ord, Show, Read, Typeable)
 
 infixr 3 :.
