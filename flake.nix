04c9c0dac964442a1860f928b480529bc4a928d8
Daniel Díaz <diaz_carrete@yahoo.com>
2023-10-08 11:22:41 +0200

    flakify

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  # https://nix.dev/anti-patterns/language#unquoted-urls
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs, ... }@attrs:
+    # https://discourse.nixos.org/t/using-nixpkgs-legacypackages-system-vs-import/17462/5
+    # https://discourse.nixos.org/t/recommendations-for-use-of-flakes-input-follows/17413
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-fmt.html
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+      # https://blog.ysndr.de/posts/guides/2021-12-01-nix-shells/#development-environments
+      # is 'devShell' (singular) actually wrong?
+      # https://nixos.wiki/wiki/Flakes
+      # https://ryantm.github.io/nixpkgs/builders/special/mkshell/
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.cabal-install 
+          (pkgs.ghcWithPackages (hpkgs : [hpkgs.tasty hpkgs.tasty-hunit])) 
+          pkgs.haskell-language-server
+          pkgs.ormolu
+          pkgs.sqlite
+        ];
+      };
+    };
+  nixConfig = {
+    # https://nixos.wiki/wiki/Flakes#Setting_the_bash_prompt_like_nix-shell
+    bash-prompt-prefix = "[develop] ";
+  };
+}
