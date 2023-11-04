2b9e647b1ccfd1c7777aed08e2bea0f897c5d6cd
Daniel Díaz <diaz_carrete@yahoo.com>
2023-11-04 09:43:02 +0100

    Change the parameter order in folds
    In order to match them with the 'foldl' library.

--- test-query/Fold.hs
+++ test-query/Fold.hs
@@ -11,11 +11,11 @@ testFolds TestEnv{..} = TestCase $ do
   execute_ conn "INSERT INTO testf (t) VALUES (4)"
   execute_ conn "INSERT INTO testf (t) VALUES (5)"
   execute_ conn "INSERT INTO testf (t) VALUES (6)"
-  val <- fold_ conn "SELECT id,t FROM testf" ([],[]) sumValues
-  assertEqual "fold_" ([3,2,1], [6,5,4]) val
-  val <- fold conn "SELECT id,t FROM testf WHERE id > ?" (MkSolo (1 :: Int)) ([],[]) sumValues
-  assertEqual "fold" ([3,2], [6,5]) val
-  val <- foldNamed conn "SELECT id,t FROM testf WHERE id > :id" [":id" := (1 :: Int)] ([],[]) sumValues
-  assertEqual "fold" ([3,2], [6,5]) val
+  val <- fold_ conn "SELECT id,t FROM testf" sumValues ([],[])
+  assertEqual "fold_"  val  ([3,2,1], [6,5,4])
+  val <- fold conn "SELECT id,t FROM testf WHERE id > ?" (MkSolo (1 :: Int))  sumValues ([],[])
+  assertEqual "fold"  val ([3,2], [6,5])
+  val <- foldNamed conn "SELECT id,t FROM testf WHERE id > :id" [":id" := (1 :: Int)]  sumValues ([],[])
+  assertEqual "fold"  val ([3,2], [6,5])
   where
-    sumValues (accId, accT) (id_ :: Int, t :: Int) = return $ (id_ : accId, t : accT)
+    sumValues (accId, accT) (id_ :: Int, t :: Int) = return (id_ : accId, t : accT)
